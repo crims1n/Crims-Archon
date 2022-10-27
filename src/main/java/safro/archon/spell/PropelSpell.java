42380bad8240b7aa5c314364ed2a7ef96f29e8b6
crims1n <pyromoosenac@gmail.com>
2022-10-27 10:41:09 -0400

    changed propel sound

--- src/main/java/safro/archon/spell/PropelSpell.java
+++ src/main/java/safro/archon/spell/PropelSpell.java
@@ -20,7 +20,8 @@ public class PropelSpell extends Spell {
     public void cast(World world, PlayerEntity player, ItemStack stack) {
         Vec3d vec = player.getRotationVector();
         player.setVelocity(0, 0.8F, 0);
-        player.addVelocity(vec.x * 0.1F + (vec.x - player.getVelocity().x), 0, vec.z * 0.1F + (vec.z - player.getVelocity().z));
+        player.addVelocity(vec.x * 0.1F + (vec.x - player.getVelocity().x), 0,
+                vec.z * 0.1F + (vec.z - player.getVelocity().z));
         player.velocityModified = true;
         player.fallDistance = 0;
     }
@@ -28,6 +29,6 @@ public class PropelSpell extends Spell {
     @Nullable
     @Override
     public SoundEvent getCastSound() {
-        return SoundEvents.ENTITY_HORSE_JUMP;
+        return SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA;
     }
 }
